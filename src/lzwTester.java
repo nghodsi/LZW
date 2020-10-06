70d2a18fe738a5e5587f319f1a73355d38128476
nghodsi <47699320+nghodsi@users.noreply.github.com>
2020-10-05 23:03:52 -0700

    Update lzwTester.java

--- src/lzwTester.java
+++ src/lzwTester.java
@@ -6,8 +6,8 @@ import java.io.*;
 public class LZWTester{
 	public static void main (String [] args) {
 		LZW cipher = new LZW ();
-		cipher.encode();
-		cipher.decode("lzwOutput.txt");
+		cipher.Encode();
+		cipher.Decode("lzwOutput.txt");
 		System.out.println(cipher.checkDecodedFile("decodedFile.txt", lzw.fileToEncodeName));
 	}
 }
