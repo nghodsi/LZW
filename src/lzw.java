1f575b8be323a0520d1367b62deeb21f83623b8e
saculgrad <70218089+saculgrad@users.noreply.github.com>
2020-10-05 22:03:30 -0700

    Final Touches
    Fixed some formatting stuff and added ascii art to both files

--- src/lzw.java
+++ src/lzw.java
@@ -1,20 +1,30 @@
+/*
+* Praise be to Ms. Kaufman and Computer Science A teachers.
+* They spoke the truth when they spoke of handwritten code and BlueJ.
+*/
+
+
 package src;
 
 import java.util.*;
 import java.io.*;
 
-public class lzw {
+public class lzw{
 
 	//TODO:
 	//Document all the things, including time complexity
-	private static String fileToEncodeName;
+	public static String fileToEncodeName;
 	//Maximum size of table (1024 for 10 bits)
 	private final int MAX_DICTIONARY_SIZE = 9999999;
 	private final int DICTIONARY_SIZE = 256;
 	
-
-	public void encode () {
-		Scanner keyboard = new Scanner(System.in); //Asks the user for the file name and saves it as a String
+	/**
+	 * Asks the user for the file they want to encode
+	 * Then uses helper encoding methods to execute the encoding process
+	 */
+	public void encode(){
+		Scanner keyboard = new Scanner(System.in); 
+		//Asks the user for the file name and saves it as a String
         System.out.print("Enter filename here: ");
         fileToEncodeName = keyboard.next();
 		keyboard.close();
@@ -22,17 +32,25 @@ public class lzw {
 		generateCodestream(fileToEncodeName, initializeDictionaryForEncode());
 	}
 
-	public void decode(String fileName) {
+	/**
+	 * Decodes the inputted the file
+	 * Uses the decoding helper methods to execute the decoding process
+	 */
+	public void decode(String fileName){
 		decodeCodestream(fileName, initializeDictionaryForDecode(), catalogCodestream(fileName));
 	}
 
 	//Initializes dictionary (ArrayList) with ASCII table <-- for Lucas & Navid's encoder
 	//O(1)
-	private ArrayList<String> initializeDictionaryForEncode() {
+	/**
+	 * Adds the first 256 (0-255) chars in the ascii table to the ArrayList dictionary
+	 * Used for encoding
+	 */
+	public ArrayList<String> initializeDictionaryForEncode(){
 		//Size 1024 for 10 bits
 		ArrayList<String> dictionary = new ArrayList<String>(MAX_DICTIONARY_SIZE);
 
-		for (int i = 0; i<256;i++) {
+		for (int i = 0; i<256;i++){
 			dictionary.add (""+(char)i);
 		}
 
@@ -41,10 +59,14 @@ public class lzw {
 
 	//Initializes dictionary (HashMap) with ASCII table <-- for decoder
 	//O(1)
-	private HashMap<Integer, String> initializeDictionaryForDecode() {
+	/**
+	 * Adds the first 256 (0-255) chars in the ascii table to theh HashMap dictionary
+	 * Used for decoding
+	 */
+	public HashMap<Integer, String> initializeDictionaryForDecode(){
 		HashMap<Integer, String> dictionary = new HashMap<Integer, String>();
 
-		for(int i = 0; i < DICTIONARY_SIZE; i++) {
+		for(int i = 0; i < DICTIONARY_SIZE; i++){
 			dictionary.put(i, "" + (char) i);
 		}
 
@@ -53,23 +75,27 @@ public class lzw {
 
 	//Generates codestream and prints it to 1st line of encoded .txt file
 	//O(n)
-	private void generateCodestream(String fileName, ArrayList<String> dictionaryAsArrayList) {
+	/**
+	 * Takes in the text file name and the arraylist dictionary
+	 * Encodes the text and outputs the codes onto a file called "lzwOutput.txt"
+	 */
+	public void generateCodestream(String fileName, ArrayList<String> dictionaryAsArrayList){
 		ArrayList<Integer> codestream = new ArrayList<Integer>();
 		String previousChar = "";
 		HashMap<String, Integer> dictionary = arrayListToHashMap(dictionaryAsArrayList);
-		try { //Tests code for errors while it's being executed
+		//Tests code for errors while it's being executed
+		try{
 			BufferedReader bReader = new BufferedReader(new FileReader(new File(fileName)));
 			//Creates output file called lzwOutput.txt
 			BufferedWriter bWriter = new BufferedWriter(new FileWriter(new File("lzwOutput.txt")));
 
-			while (bReader.ready() && dictionary.size() < MAX_DICTIONARY_SIZE) {
+			while (bReader.ready() && dictionary.size() < MAX_DICTIONARY_SIZE){
 				char currentChar = (char) bReader.read();
 
-				if (dictionary.containsKey(previousChar+currentChar)) {
+				if (dictionary.containsKey(previousChar+currentChar)){
 					previousChar=previousChar+currentChar;
 				}
-				else {
-					
+				else{
 					bWriter.write (dictionary.get(previousChar) + " ");
 					codestream.add(dictionary.get(previousChar));
 					dictionary.put(previousChar+currentChar, (Integer)(dictionary.size()));
@@ -78,66 +104,63 @@ public class lzw {
 			}
 
 			//cover last read
-			bWriter.write (dictionary.get(previousChar) + " ");
+			bWriter.write(dictionary.get(previousChar) + " ");
 			codestream.add(dictionary.get(previousChar));
 
 			//if dictionary reaches chosen bit limit
 			if(dictionary.size() >= MAX_DICTIONARY_SIZE) {
-				System.out.println("Maximum dictionary size (" +  dictionary.size() + ") reached - stopping compression");
+				System.out.println("Max dictionary size ("+dictionary.size()+") reached-stopping compression");
 
-				while(bReader.ready()) {
+				while(bReader.ready()){
 					int character = bReader.read();
 					bWriter.write(character + " ");
 					codestream.add(character);
 				}
 			}
 
-			bWriter.close();//Close readers and writers to release system resources
+			bWriter.close();
+			//Close readers and writers to release system resources
 			bReader.close();
 
 		}
-		catch (Exception e) {//Executes exception if an error is found in the try block
+		catch (Exception e){
+			//Executes exception if an error is found in the try block
 			e.printStackTrace();
 		}
 
-		//do not print codestream when testing really large files unless you have infinitely big dictionary/dictionary reset implemented
+		//do not print codestream when testing really large files
 		//System.out.println(codestream);
 		System.out.println("codestream size: " + codestream.size());
 	}
 
-	//converts an ArrayList aList to a HashMap with key-value pairs (aList[i], i).
-	private HashMap<String, Integer> arrayListToHashMap(ArrayList<String> arrayList)
-	{
+	/**
+	 * converts an ArrayList arrayList to a HashMap with key-value pairs (aList[i], i).
+	 */
+	public HashMap<String, Integer> arrayListToHashMap(ArrayList<String> arrayList){
 		HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
-		for(int i = 0; i < arrayList.size(); i++)
-		{
+		for(int i = 0; i < arrayList.size(); i++){
 			hashMap.put(arrayList.get(i), i);
 		}
 		return hashMap;
 	}
 
-
-	//Reads .txt file containing codestream and transfers all codes into an ArrayList
 	//runs in O(characters in file)
-	private ArrayList<Integer> catalogCodestream(String fileName) {
+	/**
+	 * Reads .txt file containing codestream and transfers all codes into an ArrayList
+	 */
+	public ArrayList<Integer> catalogCodestream(String fileName){
 		ArrayList<Integer> codestreamList = new ArrayList<Integer>();
-
-		try {
+		try{
 			BufferedReader codeReader = new BufferedReader(new FileReader(new File(fileName)));
-
 			String line = "";
-			if((line = codeReader.readLine()) != null) {
-				for(String code : line.split(" ")) {
-					//add character flush condition here
-
-					//add EOF condition here
-
+			if((line = codeReader.readLine()) != null){
+				for(String code : line.split(" ")){
 					codestreamList.add(Integer.parseInt(code));
 				}
 			}
 
 			codeReader.close();
-		} catch(IOException e) {
+		} catch(IOException e){
 			e.printStackTrace();
 		}
 
@@ -146,22 +169,24 @@ public class lzw {
 		return codestreamList;
 	}
 
-	//Decodes codestream and rebuilds dictionary
-	private void decodeCodestream(String fileName, HashMap<Integer, String> dictionary, ArrayList<Integer> codestreamList) {
+	/**
+	 * Rebuilds the dictionary using the arraylist of codes
+	 */
+	public void decodeCodestream(String fileName,HashMap<Integer,String>dictionary,ArrayList<Integer>codestreamList){
 		
 		String w = "" + (char) (int) codestreamList.remove(0);
 		StringBuffer decodedCodestream = new StringBuffer(w);
 		
-		for(int value : codestreamList) {
+		for(int value : codestreamList){
 			String currentEntry;
 
-			if(dictionary.containsKey(value)) {
+			if(dictionary.containsKey(value)){
 				currentEntry = dictionary.get(value);
 			}
-			else if(value == dictionary.size()) {
+			else if(value == dictionary.size()){
 				currentEntry = w + w.charAt(0);
 			}
-			else {
+			else{
 				throw new IllegalArgumentException("Bad compressed value: " + value);
 			}
 
@@ -172,44 +197,54 @@ public class lzw {
 			w = currentEntry;
 		}
 
-		try {
-			BufferedWriter decodedFileWriter = new BufferedWriter(new FileWriter(new File ("decodedFile.txt")));
-
+		try{
+			BufferedWriter decodedFileWriter=new BufferedWriter(new FileWriter(new File ("decodedFile.txt")));
 			decodedFileWriter.write(decodedCodestream.toString());
 			decodedFileWriter.close();
-		} catch(IOException e) {
+		} catch(IOException e){
 			e.printStackTrace();
 		}
 	}
 
-	//Checks decoded file with file that was originally encoded (the unencoded version)
-	private boolean checkDecodedFile(String unencodedFileName, String decodedFileName) {
-		try {
-			BufferedReader unencodedFileReader = new BufferedReader(new FileReader(new File(unencodedFileName)));
-			BufferedReader decodedFileReader = new BufferedReader(new FileReader(new File(decodedFileName)));
 
-			while(unencodedFileReader.ready() && decodedFileReader.ready()) {
-				if(unencodedFileReader.read() != decodedFileReader.read()) {
+	/**
+	 * Checks decoded file with file that was originally encoded (the unencoded version)
+	 * Makes sure that everything did what it was supposed to do
+	 */
+	public boolean checkDecodedFile(String unencodedFileName, String decodedFileName){
+		try{
+			BufferedReader unencodedFileReader=new BufferedReader(new FileReader(new File(unencodedFileName)));
+			BufferedReader decodedFileReader=new BufferedReader(new FileReader(new File(decodedFileName)));
+
+			while(unencodedFileReader.ready() && decodedFileReader.ready()){
+				if(unencodedFileReader.read() != decodedFileReader.read()){
 					unencodedFileReader.close();
 					decodedFileReader.close();
-
 					return false;
 				}
 			}
 
 			unencodedFileReader.close();
 			decodedFileReader.close();
-		} catch(IOException e) {
+		} catch(IOException e){
 			e.printStackTrace();
 		}
 
 		return true;
 	}
 
-	public static void main (String [] args) {
-		lzw cipher = new lzw ();
-		cipher.encode();
-		cipher.decode("lzwOutput.txt");
-		System.out.println(cipher.checkDecodedFile("decodedFile.txt", fileToEncodeName));
-	}
+	
 }
+//. 　　　。　　　　•　 　ﾟ　　。 　　.
+//
+//.　　　 　　.　　　　　。　　 。　. 　
+//
+//.　　 。　　　　　 ඞ 。 . 　　 • 　　　　•
+//
+//ﾟ　　 Andrew was An Impostor.　 。　.
+//
+//'　　　 1 Impostor remains 　 　　。
+//
+//ﾟ　　　.　　　. ,　　　　.　 .
+
+
